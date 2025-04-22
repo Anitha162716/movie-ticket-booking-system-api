811756c2121e18a55d061b64b9a803c296223a48
Anitha162716 <anithas2045@gmail.com>
2025-04-22 15:30:59 +0530

    project initiated

---
+++ src/main/java/com/example/movieticketbookingsystem/util/ResponseStructure.java
@@ -0,0 +1,14 @@
+package com.example.movieticketbookingsystem.util;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Builder
+public class ResponseStructure<T> {
+    private int Statuscode;
+    private String message;
+    private T data;
+
+}
