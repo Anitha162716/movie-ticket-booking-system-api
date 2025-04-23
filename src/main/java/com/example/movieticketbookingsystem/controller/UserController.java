e13338dc9d31f836d016b37c4b7e57b016f1f518
Anitha162716 <anithas2045@gmail.com>
2025-04-23 13:50:10 +0530

    pro

--- src/main/java/com/example/movieticketbookingsystem/controller/UserController.java
+++ src/main/java/com/example/movieticketbookingsystem/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.movieticketbookingsystem.controller;
 
+import com.example.movieticketbookingsystem.dto.UserRegistrationRequest;
+import com.example.movieticketbookingsystem.dto.UserResponse;
 import com.example.movieticketbookingsystem.entity.UserDetails;
 import com.example.movieticketbookingsystem.service.UserService;
 import com.example.movieticketbookingsystem.util.ResponseStructure;
@@ -9,18 +11,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/user")
+//@RequestMapping("/user")
+//@RestController
+//@AllArgsConstructor
+//public class UserController {
+//    private final UserService userService;
+//    private final RestResponseBuilder restResponseBuilder;
+//
+//    //@PostMapping("/register")
+//    //public ResponseEntity<ResponseStructure<UserDetails>> addUser(@RequestBody UserDetails user){
+//      UserDetails userDetails = userService.addUser((user));
+//    //return  restResponseBuilder.sucess(HttpStatus.OK, "New User Details Has been", userDetails);
+//
+//
+//    }
+//}
 @RestController
 @AllArgsConstructor
 public class UserController {
+
     private final UserService userService;
-    private final RestResponseBuilder restResponseBuilder;
+    private final RestResponseBuilder responseBuilder;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<UserDetails>> addUser(@RequestBody UserDetails user){
-        UserDetails userDetails = userService.addUser((user));
-        return  restResponseBuilder.sucess(HttpStatus.OK, "New User Details Has been", userDetails);
+    public ResponseEntity<ResponseStructure<UserDetails>> addUser(
+            @RequestBody UserResponse dto) {
 
+        UserDetails savedUser = userService.addUser(dto);
+        return responseBuilder.sucess(HttpStatus.OK, "User registered successfully", savedUser);
     }
-
 }
