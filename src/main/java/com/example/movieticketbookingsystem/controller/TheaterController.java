a256697016a0ec55784ef0d57112ddbafde66550
Anitha162716 <anithas2045@gmail.com>
2025-04-28 17:11:01 +0530

    Theater

---
+++ src/main/java/com/example/movieticketbookingsystem/controller/TheaterController.java
@@ -0,0 +1,29 @@
+package com.example.movieticketbookingsystem.controller;
+
+import com.example.movieticketbookingsystem.dto.TheaterRegistrationRequest;
+import com.example.movieticketbookingsystem.dto.TheaterResponse;
+import com.example.movieticketbookingsystem.service.TheaterService;
+import com.example.movieticketbookingsystem.util.ResponseStructure;
+import com.example.movieticketbookingsystem.util.RestResponseBuilder;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class TheaterController {
+    private final TheaterService theaterService;
+    private final RestResponseBuilder responseBuilder;
+
+    @PostMapping("/theaters")
+    public ResponseEntity<ResponseStructure<TheaterResponse>> addTheater(String email, @Valid @RequestBody TheaterRegistrationRequest theaterRegisterationRequest){
+        TheaterResponse theaterResponse = theaterService.addTheater(email, theaterRegisterationRequest);
+        return responseBuilder.sucess(HttpStatus.OK, "Theater has been succesfull created", theaterResponse);
+    }
+
+}
+
