98946168ef76f753ee525732fdcb40b0a9de5748
Anitha162716 <anithas2045@gmail.com>
2025-04-28 15:45:39 +0530

    update-validation-resolved

--- src/main/java/com/example/movieticketbookingsystem/controller/UserController.java
+++ src/main/java/com/example/movieticketbookingsystem/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.movieticketbookingsystem.dto.UserUpdationRequest;
 import com.example.movieticketbookingsystem.service.UserService;
 import com.example.movieticketbookingsystem.util.ResponseStructure;
 import com.example.movieticketbookingsystem.util.RestResponseBuilder;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,16 +35,18 @@ public class UserController {
     private final RestResponseBuilder responseBuilder;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<UserResponse>> addUser(@RequestBody UserRegistrationRequest user){
+    public ResponseEntity<ResponseStructure<UserResponse>> addUser(@RequestBody @Valid UserRegistrationRequest user){
         UserResponse userDetails = userService.addUser(user);
         return responseBuilder.sucess(HttpStatus.OK,"New User Details Has been added", userDetails);
     }
 
     @PutMapping("/users/{email}")
-    public ResponseEntity<ResponseStructure<UserResponse>> editUser(@PathVariable String email, @RequestBody UserUpdationRequest user){
+    public ResponseEntity<ResponseStructure<UserResponse>> editUser(@PathVariable String email, @RequestBody @Valid UserUpdationRequest user){
         UserResponse userDetails = userService.editUser(user, email);
         return responseBuilder.sucess(HttpStatus.OK,"User Details has been updated", userDetails);
     }
+
+
     @DeleteMapping("/users/{email}")
     public ResponseEntity<ResponseStructure<UserResponse>> softDeleteUser(@PathVariable String email){
         UserResponse userDetails = userService.softDeleteUser(email);
